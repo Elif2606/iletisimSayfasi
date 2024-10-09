26c603669e6d9de873ad3b1659c755b8557980e4
Elif2606 <126257830+Elif2606@users.noreply.github.com>
2024-10-09 21:38:57 +0300

    Delete teknoloji directory

--- teknoloji/script.js
+++
@@ -1,8 +0,0 @@
-// "Daha Fazlasını Keşfet" butonuna tıklandığında içerik bölümünü göster
-document.getElementById("learn-more-btn").addEventListener("click", function() {
-    const contentSection = document.querySelector('.content');
-    contentSection.classList.toggle('hidden');
-
-    // Sayfa kaydırma animasyonu
-    contentSection.scrollIntoView({ behavior: 'smooth' });
-});
